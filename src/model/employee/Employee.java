5794d829815fc801a43fa85d2b050bbb9aeb2761
Dao Thi Yen <daoy993@gmail.com>
2026-06-14 02:09:00 +0700

    update

--- src/model/employee/Employee.java
+++ src/model/employee/Employee.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.employee;
 
-/**
- *
- * @author FPTSHOP
- */
 public class Employee {
-    
+
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String username;
+    private String password;
+
+    public Employee(String employeeId,
+                    String fullName,
+                    String email,
+                    String username,
+                    String password) {
+
+        this.employeeId = employeeId;
+        this.fullName = fullName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
