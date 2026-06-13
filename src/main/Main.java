0cf2e91b79072ebbfd1c4c747e59facece5bf13a
Tên của bạn <dinhdaongoc18@gmail.com>
2026-06-14 00:39:20 +0700

    1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author FPTSHOP
- */
+import view.auth.LoginFrame;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        new LoginFrame();
+    }
 }
