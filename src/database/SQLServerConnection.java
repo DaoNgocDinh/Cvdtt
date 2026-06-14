ff81f08006061e7f23d2e1a7c39547d0047714ff
Helishjs <caothaituan2005@gmail.com>
2026-06-15 01:42:15 +0700

    FIN

--- src/database/SQLServerConnection.java
+++ src/database/SQLServerConnection.java
@@ -1,44 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ *
+ * @author FPTSHOP
+ */
 public class SQLServerConnection {
 
-    private static final String DRIVER_CLASS
-            = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    // Default values set to user's local instance (use host:port to avoid instance resolution); can be overridden with system properties
+    private static final String SERVER = "DESKTOP-B4J24OU:1433";
+    private static final String DATABASE = "cvdtt1";
+    private static final String USERNAME = "sa";
+    private static final String PASSWORD = "123456";
 
-    public static Connection getConnection()
-            throws ClassNotFoundException, SQLException {
-
+    /**
+     * Returns a JDBC connection. Behavior can be configured via system properties:
+     * -db.server (default: localhost)
+     * -db.database (default: cvdtt1)
+     * -db.useIntegratedSecurity (true|false) when true uses Windows Integrated Authentication
+     * -db.username -db.password used when integrated security is false
+     *
+     * Note: Windows Integrated Authentication requires the Microsoft sqljdbc_auth.dll
+     * on the JVM library path (see driver distribution auth/x64 or auth/x86 folders).
+     */
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName(DRIVER_CLASS);
 
-        String url
-                = "jdbc:sqlserver://localhost:1433;"
-                + "databaseName=cvdtt1;"
-                + "integratedSecurity=true;"
-                + "encrypt=false;"
-                + "trustServerCertificate=true;";
+        String server = System.getProperty("db.server", SERVER);
+        String database = System.getProperty("db.database", DATABASE);
+        boolean useIntegrated = Boolean.parseBoolean(System.getProperty("db.useIntegratedSecurity", "false"));
 
-        return DriverManager.getConnection(url);
-    }
+        String url = String.format("jdbc:sqlserver://%s;databaseName=%s;encrypt=false;trustServerCertificate=true", server, database);
 
-    public static void main(String[] args) {
-        try (Connection conn = getConnection()) {
-
-            if (conn != null && !conn.isClosed()) {
-                System.out.println("✅ Kết nối SQL Server thành công!");
-                System.out.println("Database: " + conn.getCatalog());
-            }
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("❌ Không tìm thấy JDBC Driver!");
-            e.printStackTrace();
-
-        } catch (SQLException e) {
-            System.out.println("❌ Kết nối SQL Server thất bại!");
-            e.printStackTrace();
+        if (useIntegrated) {
+            // Use Windows Integrated Authentication. Requires sqljdbc_auth.dll available to the JVM.
+            url = url + ";integratedSecurity=true";
+            return DriverManager.getConnection(url);
+        } else {
+            String username = System.getProperty("db.username", USERNAME);
+            String password = System.getProperty("db.password", PASSWORD);
+            return DriverManager.getConnection(url, username, password);
         }
     }
 }
